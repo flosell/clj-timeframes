f851e0870b036e31e820e06b22c9bf16adb82c93
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-11-15 12:37:31 +0100

    add description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-timeframes "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A small library to merge overlapping clj-time intervals"
+  :url "https://github.com/flosell/clj-timeframes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
