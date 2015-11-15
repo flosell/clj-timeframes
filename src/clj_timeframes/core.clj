7f164fb9e3f5ce63c4409e6fcbe23c603fe13d0a
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-11-15 10:38:24 +0100

    basic algorithm

--- src/clj_timeframes/core.clj
+++ src/clj_timeframes/core.clj
@@ -1,6 +1,24 @@
-(ns clj-timeframes.core)
+(ns clj-timeframes.core
+  (:require [clj-time.core :as t]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- merge-interval [earlier-start-interval later-end-interval]
+  (t/interval (t/start earlier-start-interval) (t/end later-end-interval)))
+
+
+(defn- should-merge? [a b]
+  (or
+    (t/overlaps? a b)
+    (t/abuts? a b)))
+
+(defn-  merge-internal [current sorted-remaining]
+  (cond
+    (empty? sorted-remaining) #{current}
+    (should-merge? current (first sorted-remaining)) (let [new-cur (merge-interval current (first sorted-remaining))
+                                                         rest-result (merge-internal new-cur (rest sorted-remaining))]
+                                                     rest-result)
+    :else (let [rest-result (merge-internal (first sorted-remaining) (rest sorted-remaining))]
+                                                           (conj rest-result current))))
+
+(defn merge-intervals [intervals]
+  (let [sorted (sort-by t/start intervals)]
+    (merge-internal (first sorted) (rest sorted))))
--- test/clj_timeframes/core_test.clj
+++ test/clj_timeframes/core_test.clj
@@ -1,7 +1,29 @@
 (ns clj-timeframes.core-test
   (:require [clojure.test :refer :all]
-            [clj-timeframes.core :refer :all]))
+            [clj-timeframes.core :refer :all]
+            [clj-time.core :as t]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def start-time (t/now))
+
+(def after-5-sec (t/plus start-time (t/seconds 10)))
+(def after-ten-sec (t/plus start-time (t/seconds 10)))
+(def after-fifteen-sec (t/plus start-time (t/seconds 15)))
+(def after-twenty-sec (t/plus start-time (t/seconds 20)))
+
+
+(deftest merge-intervals-test
+  (testing "that a single interval is unchanged"
+    (is (= #{(t/interval start-time after-ten-sec)}
+           (merge-intervals [(t/interval start-time after-ten-sec)]))))
+  (testing "that two intervals that dont overlap stay in the end result"
+    (is (= #{(t/interval start-time after-ten-sec)
+             (t/interval after-fifteen-sec after-twenty-sec)}
+           (merge-intervals [(t/interval start-time after-ten-sec)
+                             (t/interval after-fifteen-sec after-twenty-sec)]))))
+  (testing "that intervals that do overlap are merged together"
+    (is (= #{(t/interval start-time after-twenty-sec)}
+           (merge-intervals [(t/interval start-time after-ten-sec)
+                             (t/interval after-5-sec after-twenty-sec)])))
+    (is (= #{(t/interval start-time after-twenty-sec)}
+           (merge-intervals [(t/interval after-5-sec after-twenty-sec)
+                             (t/interval start-time after-ten-sec)])))))
